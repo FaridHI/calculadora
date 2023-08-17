cfb999c5317c68f8729ef9b6f2b7a95f20635b32
FaridHI <1121041@senati.pe>
2023-08-17 11:22:18 -0500

    agregue resta

--- Calculadora.java
+++ Calculadora.java
@@ -23,5 +23,17 @@ public class Calculadora {
                int suma = numero1 + numero2;
                    System.out.println("la suma de " + numero1 + " + " + numero2 + " es: " + suma);
                System.out.println();
+               
+               System.out.println("La Resta");
+               System.out.print("INGRESE EL PRIMER NUMERO: ");
+               
+               int numero3 = scanner.nextInt();
+               
+               System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+               int numero4 = scanner.nextInt();
+               
+               int resta = numero3 - numero4;
+                   System.out.println("la resta de " + numero3 + " - " + numero4 + " es: " + resta);
+               
     }
 }
