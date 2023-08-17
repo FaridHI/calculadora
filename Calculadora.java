75e5a3dfe09a6349370a1f9d9561d965d85d360d
FaridHI <1121041@senati.pe>
2023-08-17 11:18:32 -0500

    agregue suma

--- Calculadora.java
+++ Calculadora.java
@@ -11,6 +11,17 @@ import java.util.Scanner;
 public class Calculadora {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+               System.out.println("La suma");
+               System.out.print("INGRESE EL PRIMER NUMERO: ");
+               
+               int numero1 = scanner.nextInt();
+               
+               System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+               int numero2 = scanner.nextInt();
+               
+               int suma = numero1 + numero2;
+                   System.out.println("la suma de " + numero1 + " + " + numero2 + " es: " + suma);
+               System.out.println();
     }
 }
