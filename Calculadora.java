2c549553c4e5dcaf2add9e2ea097f6d40ed268dd
FaridHI <1121041@senati.pe>
2023-08-17 11:04:33 -0500

    Cree un archivo java e ingrese un scanner

---
+++ Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+import java.util.Scanner;
+/**
+ *
+ * @author Alumno
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        
+    }
+}
